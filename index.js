f513e48530876afaed00a7d9b1023a1ec26ad9e2
tiago <tiago@ticogame.com>
2020-07-05 15:51:54 -0300

    foi

--- index.js
+++ index.js
@@ -1,56 +1,52 @@
-const KEY = 'express.sid',
-    SECRET = 'express',
-    express = require("express"),
-    app = express(),
-    Fs = require('fs'),
-    options = {
-        key: Fs.readFileSync('key.pem'),
-        cert: Fs.readFileSync('cert.pem')
-    },
-    server = require("https").createServer(options, app),
-    io = require("socket.io").listen(server),
-    cookie = express.cookieParser(SECRET)
-    , store = new express.session.MemoryStore()
+ // app.js
+ const KEY = 'express.sid'
+   , SECRET = 'express';
+ var express = require('express')
+   , app = express()
+   , server = require('http').createServer(app)
+   , io = require('socket.io').listen(server)
+   , cookie = express.cookieParser(SECRET)
+   , store = new express.session.MemoryStore()
    , session = express.session({secret: SECRET
                               , key: KEY
                               , store: store});
-
-
-    app.configure(()=>{
-        app.set('view engine', 'ejs');
-        app.use(cookie);
-        app.use(session);
-    })
-
-rotas=require("src/routes")(app)
-server.listen(3001, function(){
-    console.log("Express e Socket.IO no ar.");
-  });
-
-   // Configurações do Socket.IO
+ // Configurações de Cookie e Session do Express
+ app.configure(function(){
+   app.set('view engine', 'ejs');
+   app.use(cookie);
+   app.use(session);
+ });
+ app.get("/", function(req, res){
+   req.session.nome = "Caio";
+   res.render('index');
+ });
+ server.listen(3000, function(){
+   console.log("Express e Socket.IO no ar.");
+ });
+ // Configurações do Socket.IO
  io.set('authorization', function(data, accept) {
-    cookie(data, {}, function(err) {
-      if (!err) {
-        var sessionID = data.signedCookies[KEY];
-        store.get(sessionID, function(err, session) {
-          if (err || !session) {
-            accept(null, false);
-          } else {
-            data.session = session;
-            accept(null, true);
-          }
-        });
-      } else {
-        accept(null, false);
-      }
-    });
-  });
-  io.sockets.on('connection', function (client) {
-    var session = client.handshake.session
-      , nome = session.nome;
-    client.on('toServer', function (msg) {
-      msg = "<b>"+nome+":</b> "+msg+"<br>";
-      client.emit('toClient', msg);
-      client.broadcast.emit('toClient', msg);
-    });
-  });
+   cookie(data, {}, function(err) {
+     if (!err) {
+       var sessionID = data.signedCookies[KEY];
+       store.get(sessionID, function(err, session) {
+         if (err || !session) {
+           accept(null, false);
+         } else {
+           data.session = session;
+           accept(null, true);
+         }
+       });
+     } else {
+       accept(null, false);
+     }
+   });
+ });
+ io.sockets.on('connection', function (client) {
+   var session = client.handshake.session
+     , nome = session.nome;
+   client.on('toServer', function (msg) {
+     msg = "<b>"+nome+":</b> "+msg+"<br>";
+     client.emit('toClient', msg);
+     client.broadcast.emit('toClient', msg);
+   });
+ });
