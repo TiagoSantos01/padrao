51528bee084b656e98e07ee4bb10e7f3ee944fc8
tiago <tiago@ticogame.com>
2020-07-05 16:31:01 -0300

    foi

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const Chave = 'sessao',
     Store = new ExpressSession.MemoryStore();
 
 App.set('views', __dirname + '/views');
-App.use(Express.static(__dirname));
+App.use(Express.static(__dirname + '/public'));
 App.set('view engine', 'ejs');
 App.use(Cookie);
 App.use(ExpressSession({
